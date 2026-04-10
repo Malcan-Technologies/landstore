2a54d8f6981ec888c1d39bec30e3b0e18d6b5ea0
basitshykh <basitshykh8@gmail.com>
2026-04-10 21:52:19 +0500

    add type definitions for Multer file handling in Express

---
+++ server/types/express/index.d.ts
@@ -0,0 +1,20 @@
+import 'express'
+
+declare global {
+  namespace Express {
+    namespace Multer {
+      interface File {
+        fieldname: string
+        originalname: string
+        encoding: string
+        mimetype: string
+        size: number
+        buffer: Buffer
+      }
+    }
+
+    interface Request {
+      files?: Multer.File[]
+    }
+  }
+}
